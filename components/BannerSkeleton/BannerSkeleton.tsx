0b3e671eaf886d5d6c572a8d8043f6fc8d52355c
ShaYha271097 <shayha271097@gmail.com>
2026-05-31 00:21:47 +0700

    llllssslsssâaaaa

--- components/BannerSkeleton/BannerSkeleton.tsx
+++ components/BannerSkeleton/BannerSkeleton.tsx
@@ -2,12 +2,19 @@ export default function BannerSkeleton() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-3 animate-pulse">
       {/* LEFT */}
-      <div className="lg:col-span-2 h-[250px] sm:h-[320px] lg:h-[400px] rounded-[10px] bg-gray-200" />
+      <div className="lg:col-span-2 h-[250px] sm:h-[320px] lg:h-[400px] rounded-[10px] overflow-hidden bg-gray-100 p-3">
+        <div className="w-full h-full rounded-[10px] bg-gray-200" />
+      </div>
 
       {/* RIGHT */}
       <div className="flex lg:flex-col gap-3 h-[150px] sm:h-[200px] lg:h-[400px]">
-        <div className="flex-1 rounded-[10px] bg-gray-200" />
-        <div className="flex-1 rounded-[10px] bg-gray-200" />
+        <div className="flex-1 rounded-[10px] overflow-hidden bg-gray-100 p-3">
+          <div className="w-full h-full rounded-[10px] bg-gray-200" />
+        </div>
+
+        <div className="flex-1 rounded-[10px] overflow-hidden bg-gray-100 p-3">
+          <div className="w-full h-full rounded-[10px] bg-gray-200" />
+        </div>
       </div>
     </div>
   );
