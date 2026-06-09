aa81042926bf7283966514f0ef364378525e68ed
ShaYha271097 <shayha271097@gmail.com>
2026-06-09 12:46:49 +0700

    ....11111112222

--- components/SearchBox/SearchBox.tsx
+++ components/SearchBox/SearchBox.tsx
@@ -1,27 +1,35 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useRouter } from "next/navigation";
 
 export default function SearchBox() {
   const [keyword, setKeyword] = useState("");
   const router = useRouter();
+  const inputRef = useRef<HTMLInputElement>(null);
 
-  const handleSearch = (e: any) => {
+  const handleSearch = (e: React.FormEvent) => {
     e.preventDefault();
 
-    if (!keyword.trim()) return;
+    const value = keyword.trim();
 
-    router.push(`/products?search=${encodeURIComponent(keyword.trim())}`);
+    if (!value) return;
+
+    // Ẩn bàn phím mobile
+    inputRef.current?.blur();
+
+    router.push(
+      `/products?search=${encodeURIComponent(value)}`
+    );
   };
-
 
   return (
     <form
       onSubmit={handleSearch}
-      className="frm_timkiem timkiem_header_mobile  timkiem_header_des "
+      className="frm_timkiem timkiem_header_mobile timkiem_header_des"
     >
       <input
+        ref={inputRef}
         type="text"
         className="input"
         placeholder="Nhập từ khóa cần tìm..."
